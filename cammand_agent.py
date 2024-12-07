e99736682bd12d0b20ac6c3ec671833964fe3d94
Ghilth <ghilthg@gmail.com>
2024-12-07 09:19:11 +0100

    Add prompt and class names

--- cammand_agent.py
+++ cammand_agent.py
@@ -80,18 +80,42 @@ class Agent:
     
 
 
+
+#Yolo class names
+class_list = [
+    "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", 
+    "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", 
+    "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", 
+    "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", 
+    "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", 
+    "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", 
+    "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", 
+    "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch", 
+    "potted plant", "bed", "dining table", "toilet", "TV", "laptop", "mouse", 
+    "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", 
+    "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", 
+    "toothbrush"
+]
+
+
+
 #Prompt
 
-prompt="""You are smart tracking assistant.Use the track tool to loop up information.\
+prompt=f"""You are smart tracking assistant.Use the track tool to loop up information.\
 You are allowed to make multiple calls (either together or in sequence).\
 Only look up information when you are sure of what you want.\
 If you need to look up some information before asking a follow up question,you are allowed to do that!
+
+
+If a class asked by the user doesn't exist in this list of class {class_list}, say that a model is not able to find this class.
+
 """
+
 model=ChatGoogleGenerativeAI(model="gemini-1.5-pro-latest",google_api_key=key)
 TrackMindBot=Agent(model,[tool],system=prompt)
 
 
 
 
-messages =[HumanMessage(content="Find me chair object of  frames")]
+messages =[HumanMessage(content="Find me chair object on  frames")]
 result=TrackMindBot.graph.invoke({"messages":messages})
